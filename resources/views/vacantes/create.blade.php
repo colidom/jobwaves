a79292375424d8c35ab9739424005d795b9a33bd
colidom <colidom@outlook.com>
2024-03-05 22:39:15 +0100

    Instalando Livewire

--- resources/views/vacantes/create.blade.php
+++ resources/views/vacantes/create.blade.php
@@ -8,9 +8,14 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-700 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-700 dark:text-gray-100">
-                    {{ __('Crear Vacante') }}
+                    <h1 class="text-2xl font-bold text-center mb-10">
+                        Publicar Vacante
+                    </h1>
+                    <div class="md:flex md:justify-center p-5">
+                        <livewire:create-vacancy />
+
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
 </x-app-layout>
