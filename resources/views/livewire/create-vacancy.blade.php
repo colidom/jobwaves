27c6ac51789fa4673a985ac1e788ef8a654a077c
colidom <colidom@outlook.com>
2024-03-05 23:28:24 +0100

    Añadiendo los Campos

--- resources/views/livewire/create-vacancy.blade.php
+++ resources/views/livewire/create-vacancy.blade.php
@@ -1,3 +1,61 @@
-<div>
-    <h1>Desde livewire</h1>
-</div>
+<form action="" class="md:w-1/2 space-y-5">
+    <!-- Titulo -->
+    <div>
+        <x-input-label for="title" :value="__('Titulo')" />
+        <x-text-input id="title" class="block mt-1 w-full" type="text" name="title" :value="old('title')"
+            placeholder="Titulo Vacante" />
+        <x-input-error :messages="$errors->get('title')" class="mt-2" />
+    </div>
+
+    <!-- Salario -->
+    <div>
+        <x-input-label for="salary" :value="__('Salario')" />
+        <select name="salary" id="salary"
+            class="border-gray-300 mt-1 dark:border-gray-700 dark:bg-gray-100 dark:text-gray-700 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm w-full">
+        </select>
+        <x-input-error :messages="$errors->get('title')" class="mt-2" />
+    </div>
+
+    <!-- Categoría -->
+    <div>
+        <x-input-label for="category" :value="__('Categoría')" />
+        <select name="category" id="category"
+            class="border-gray-300 mt-1 dark:border-gray-700 dark:bg-gray-100 dark:text-gray-700 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm w-full">
+        </select>
+        <x-input-error :messages="$errors->get('title')" class="mt-2" />
+    </div>
+
+    <!-- Nombre empresa -->
+    <div>
+        <x-input-label for="company_name" :value="__('Empresa')" />
+        <x-text-input id="company_name" class="block mt-1 w-full" type="text" name="company_name" :value="old('company_name')"
+            placeholder="Nombre de la Empresa" />
+        <x-input-error :messages="$errors->get('company_name')" class="mt-2" />
+    </div>
+
+    <!-- Último día -->
+    <div>
+        <x-input-label for="end_date" :value="__('Último día para inscribirse')" />
+        <x-text-input id="end_date" class="block mt-1 w-full" type="date" name="end_date" :value="old('end_date')" />
+        <x-input-error :messages="$errors->get('end_date')" class="mt-2" />
+    </div>
+
+    <!-- Descripción -->
+    <div>
+        <x-input-label for="job_description" :value="__('Descripción')" />
+        <textarea id="job_description"
+            class="border-gray-300 mt-1 dark:border-gray-700 dark:bg-gray-100 dark:text-gray-700 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm w-full h-72"
+            type="text" name="job_description" :value="old('job_description')"
+            placeholder="Descripción general del puesto de trabajo"></textarea>
+        <x-input-error :messages="$errors->get('job_description')" class="mt-2" />
+    </div>
+
+    <!-- Imagen -->
+    <div>
+        <x-input-label for="image" :value="__('Imagen')" />
+        <x-text-input id="image" class="block mt-1 w-full" type="file" name="image" />
+        <x-input-error :messages="$errors->get('image')" class="mt-2" />
+    </div>
+
+    <x-primary-button class="w-full justify-center">Crear puesto</x-primary-button>
+</form>
--- resources/views/vacantes/create.blade.php
+++ resources/views/vacantes/create.blade.php
@@ -8,7 +8,7 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-100 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-700 dark:text-gray-900">
-                    <h1 class="text-2xl font-bold text-center mb-10">
+                    <h1 class="text-2xl font-bold text-center my-10">
                         Publicar Vacante
                     </h1>
                     <div class="md:flex md:justify-center p-5">
