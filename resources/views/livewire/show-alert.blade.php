9be426a77b171392d34249743c32103dad06e972
colidom <colidom@outlook.com>
2024-03-10 23:23:22 +0100

    Añadidos estilos a mensajes de alerta

--- resources/views/livewire/show-alert.blade.php
+++ resources/views/livewire/show-alert.blade.php
@@ -1,3 +1,3 @@
-<div class="border border-red-500 bg-red-100 text-red-700 font-bold uppercase p-2 mt-2 text-xs">
+<div class="border border-red-500 rounded bg-red-100 text-red-700 font-bold uppercase p-2 mt-2 text-xs">
     {{ $message }}
 </div>
--- resources/views/vacantes/index.blade.php
+++ resources/views/vacantes/index.blade.php
@@ -7,7 +7,8 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             @if (session()->has('message'))
-                <div class="uppercase border border-green-600 bg-green-100 text-green-600 font-bold p-2 my-3 text-sm">
+                <div
+                    class="uppercase border rounded border-green-600 bg-green-100 text-green-600 font-bold p-2 my-3 text-sm">
                     {{ session('message') }}
                 </div>
             @endif
