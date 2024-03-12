2a6cb06838d7145b3bda658ae6e746fb449f171d
colidom <colidom@outlook.com>
2024-03-12 16:34:49 +0100

    Añadidas clases taiwind en componente show-alert

--- resources/views/livewire/show-alert.blade.php
+++ resources/views/livewire/show-alert.blade.php
@@ -1,3 +1,3 @@
-<div class="border border-red-500 rounded bg-red-100 text-red-700 font-bold uppercase p-2 mt-2 text-xs">
+<div class="bg-red-100 border-l-4 border-red-600 text-red-600 font-bold p-3 mt-2 text-xs">
     {{ $message }}
 </div>
