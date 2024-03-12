92e795ccc6e1055d5be64cf621adbd05e52ddca9
colidom <colidom@outlook.com>
2024-03-12 17:15:37 +0100

    Editando la vacante

--- app/Livewire/EditVacancy.php
+++ app/Livewire/EditVacancy.php
@@ -10,6 +10,7 @@ use Livewire\Component;
 
 class EditVacancy extends Component
 {
+    public $vacancy_id;
     public $title;
     public $salary;
     public $category;
@@ -30,6 +31,7 @@ class EditVacancy extends Component
     // Muestra los datos en el formulario de la instancia Vacancy
     public function mount(Vacancy $vacancy)
     {
+        $this->vacancy_id = $vacancy->id;
         $this->title = $vacancy->title;
         $this->salary = $vacancy->salary_id;
         $this->category = $vacancy->category_id;
@@ -42,6 +44,28 @@ class EditVacancy extends Component
     public function editVacancy()
     {
         $data = $this->validate();
+
+        // Revisar si hay una nueva imagen
+
+        // Encontrar vacante a editar
+        $vacancy = Vacancy::Find($this->vacancy_id);
+
+        // Asignar los valores
+        $vacancy->title = $data['title'];
+        $vacancy->salary_id = $data['salary'];
+        $vacancy->category_id = $data['category'];
+        $vacancy->company_name = $data['company_name'];
+        $vacancy->end_date = Carbon::createFromFormat('Y-m-d', $data['end_date'])->format('Y-m-d');
+        $vacancy->job_description = $data['job_description'];
+
+        // Guardar la vacante
+        $vacancy->save();
+
+        // Crear mensaje
+        session()->flash('message', 'Oferta de trabajo actualizada correctamente');
+
+        // Redireccionar
+        return redirect()->route('vacantes.index');
     }
 
     public function render()
