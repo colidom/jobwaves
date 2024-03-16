d0e0e94ec1f9df98168e23c2c59ab7711e9592f9
colidom <colidom@outlook.com>
2024-03-16 15:44:03 +0100

    Ajustes de colores en las distintas vistas

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -25,7 +25,7 @@
         </div>
 
         <div
-            class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white dark:bg-gray-100 shadow-md overflow-hidden sm:rounded-lg">
+            class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white dark:bg-gray-200 shadow-md overflow-hidden sm:rounded-lg">
             {{ $slot }}
         </div>
     </div>
