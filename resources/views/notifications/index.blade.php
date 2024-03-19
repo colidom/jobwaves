f29606affc68df28cfd37504f37a2c19cce4cf53
colidom <colidom@outlook.com>
2024-03-19 19:01:24 +0100

    Añadidas clases a enlace

--- resources/views/notifications/index.blade.php
+++ resources/views/notifications/index.blade.php
@@ -24,8 +24,14 @@
                             </div>
                             <div class="mt-5 lg:mt-0">
                                 <a href="#"
-                                    class="bg-indigo-500 p-3 text-sm uppercase font-bold text-white rounded-lg">Ver
-                                    candidatos</a>
+                                    class="inline-flex items-center px-4 py-2 bg-indigo-600 border
+                                    border-transparent rounded-md font-semibold text-xs text-white uppercase
+                                    tracking-widest hover:bg-indigo-700 focus:bg-indigo-700 dark:focus:bg-indigo
+                                    active:bg-indigo-900 dark:active:bg-indigo-700 focus:outline-none focus:ring-2
+                                    focus:ring-indigo-500 focus:ring-offset-2 dark:focus:ring-offset-indigo-200
+                                    transition ease-in-out duration-150">
+                                    Ver candidatos
+                                </a>
                             </div>
                         </div>
                     @empty
