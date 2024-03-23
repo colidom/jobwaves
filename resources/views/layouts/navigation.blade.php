6784f521420ded31a3e2e9c01880c2a030892201
colidom <colidom@outlook.com>
2024-03-23 11:00:19 +0100

    Quitado espacio en clases

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -12,7 +12,7 @@
 
                 <!-- Icono Home-->
                 <div
-                    class="inline-flex items-center px-1 pt-1  ml-5 border-b-2 border-transparent text-sm font-medium leading-5 text-gray-500
+                    class="inline-flex items-center px-1 pt-1 ml-5 border-b-2 border-transparent text-sm font-medium leading-5 text-gray-500
                     dark:text-gray-500 hover:text-gray-700 dark:hover:text-gray-900 hover:border-gray-900 dark:hover:border-gray-700
                     focus:outline-none focus:text-gray-700 dark:focus:text-gray-900 focus:border-gray-300 dark:focus:border-gray-700
                     transition duration-150 ease-in-out">
