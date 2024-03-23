5c3cd376354449d41871615fb61076c964e5f313
colidom <colidom@outlook.com>
2024-03-23 16:15:22 +0100

    Ajustes en textos

--- resources/views/livewire/home-vacancies.blade.php
+++ resources/views/livewire/home-vacancies.blade.php
@@ -4,7 +4,7 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto">
             <h3 class="font-extrabold text-4xl text-gray-700 mb-12">
-                Nuestras ofertas de empleo disponibles
+                {{ __('Nuestras ofertas de empleo disponibles') }}
             </h3>
             <div class="bg-white shadow-sm rounded-lg p-6 divide-y divide-gray-200">
                 @forelse ($vacancies as $vacancy)
@@ -16,7 +16,7 @@
                             <p class="text-base text-gray-600 mb-1">{{ $vacancy->company_name }}</p>
                             <p class="text-base text-gray-600 mb-1">{{ $vacancy->salary->salary }}</p>
                             <p class="text-xs font-bold text-gray-600 mb-1">{{ $vacancy->category->category }}</p>
-                            <p class="font-bold text-xs text-gray-600">Último día para inscribirte:
+                            <p class="font-bold text-xs text-gray-600">{{ __('Último día para inscribirse:') }}
                                 <span class="font-normal">{{ $vacancy->end_date->format('d/m/Y') }}</span>
                             </p>
                         </div>
@@ -27,13 +27,15 @@
                             active:bg-indigo-900 dark:active:bg-indigo-700 focus:outline-none focus:ring-2
                             focus:ring-indigo-500 focus:ring-offset-2 dark:focus:ring-offset-indigo-200
                             transition ease-in-out duration-150"
-                                href="{{ route('vacancies.show', $vacancy->id) }}">Ver
-                                oferta de empleo</a>
+                                href="{{ route('vacancies.show', $vacancy->id) }}">
+                                {{ __('Ver oferta de empleo') }}
+                            </a>
                         </div>
                     </div>
                 @empty
-                    <p class="p-3 text-center text-sm text-gray-600">No existen ofertas de empleo con estos criterios de
-                        búsqueda.</p>
+                    <p class="p-3 text-center text-sm text-gray-600">
+                        {{ __('No existen ofertas de empleo con estos criterios de búsqueda.') }}
+                    </p>
                 @endforelse
             </div>
         </div>
