bd00c9607e6f644a30523f78e8313d2e08eca155
colidom <colidom@outlook.com>
2024-04-02 15:16:47 +0200

    Cambiado texto de pantalla principal

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -3,7 +3,7 @@
         <div class=" max-w-xl mx-auto px-4 sm:px-6 lg:px-8 lg:max-w-7xl">
             <div class="relative">
                 <h2 class="text-center text-4xl leading-8 font-extrabold tracking-tight text-indigo-600 sm:text-6xl">
-                    {{ __('Encuentra un empleo en el sector IT de forma remota') }}</h2>
+                    {{ __('Encuentra un empleo en el sector IT') }}</h2>
                 <p class="mt-4 max-w-3xl mx-auto text-center text-xl text-gray-500">
                     {{ __('Encuentra la oportunidad profesional de tus sueños en una destacada empresa del sector de Tecnologías de la Información (IT).') }}
                 </p>
